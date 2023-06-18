327d5d250319f56d77694dec952e2418c4b7554d
Mehmood Khan <mehmadk05@gmail.com>
2023-06-18 14:50:34 -0700

    changes in homepage and design changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -83,186 +83,153 @@ function Home() {
         </button>
       </div>
 
-      {/* <form method="POST">
-        <div className="container filter d-md-flex rounded">
-          <select
-            className="ms-3 col-4 captionDesg rounded"
-            name="loc_id"
-            onChange={handleChange}
-          >
-            {locations.map((location, key) => (
-              <option value={location.loc_id} key={key}>
-                {location.loc_name}
-              </option>
-            ))}
-          </select>
-          <select
-            className="ms-3 col-3 captionDesg rounded "
-            name="type"
-            onChange={handleChange}
-          >
-            <option value="1">Rent</option>
-            <option value="2">Buy</option>
-          </select>
-          <select
-            className="ms-3 col-3 captionDesg rounded "
-            name="property_type"
-            onChange={handleChange}
-          >
-            <option value="1">House</option>
-            <option value="2">Room</option>
-            <option value="3">Flat</option>
-            <option value="4">Plot</option>
-          </select>
-
-          <button
-            onClick={handleSubmit}
-            className="btn btn-info btn-outline-dark ms-3 text-light py-2"
-          >
-            Filter Search
-          </button>
-        </div>
-      </form> */}
-
-      <div className="container-fluid mb-5 p-5 whiteBackground">
-        <div className="row">
-          <div className="col-sm-6">
-            <h1 className="mx-5 mt-5 fw-bold text-center">Houses</h1>
-            <p className="mt-4 ms-5" style={{ textAlign: "justify" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-              laboriosam error a voluptatem voluptatum consequatur, porro
-              delectus iste pariatur quos illo numquam exercitationem tempora
-              animi est laudantium quis quaerat laborum accusantium accusamus
-              distinctio adipisci libero omnis minima. Laboriosam, quas,
-              voluptatem sed dolore temporibus aut iure optio, atque ipsum
-              corrupti amet voluptate vitae id ratione possimus. Esse et rem
-              accusamus maxime consectetur at, dolor porro impedit beatae qui
-              animi debitis repellendus quo, omnis doloremque corrupti,
-              similique fuga possimus inventore neque ea.
-            </p>
-            <div align="center">
-              <Link
-                to="/houses"
-                className="btn btn-outline-primary rounded-pill px-3 py-2 mt-5"
-              >
-                View All Houses
-              </Link>
+      <section className="houses-section whiteBackground pt-5">
+        <div className="container mb-5">
+          <div className="row">
+            <h1 className="mt-5 fw-bold text-center">Houses</h1>
+            <div className="col-lg-6 pt-5">
+              <p className="mt-4 ms-5" style={{ textAlign: "justify" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                laboriosam error a voluptatem voluptatum consequatur, porro
+                delectus iste pariatur quos illo numquam exercitationem tempora
+                animi est laudantium quis quaerat laborum accusantium accusamus
+                distinctio adipisci libero omnis minima. Laboriosam, quas,
+                voluptatem sed dolore temporibus aut iure optio, atque ipsum
+                corrupti amet voluptate vitae id ratione possimus. Esse et rem
+                accusamus maxime consectetur at, dolor porro impedit beatae qui
+                animi debitis repellendus quo, omnis doloremque corrupti,
+                similique fuga possimus inventore neque ea.
+              </p>
+              <div align="center">
+                <Link
+                  to="/houses"
+                  className="btn btn-outline-warning text-dark rounded px-5 py-2 mt-4 text-uppercase"
+                >
+                  View All Houses
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="col-sm-6 img-hover-zoom img-hover-zoom--brightness">
-            <img
-              src="./images/5.jpg"
-              className="rounded w-100 ms-4 mt-5 imgShadow"
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="container-fluid p-5 greyBackground">
-        <div className="row">
-          <div className="col-sm-6 img-hover-zoom img-hover-zoom--brightness">
-            <img
-              src="./images/a.jpg"
-              className="rounded w-100 ms-4 mt-5 imgShadow"
-              alt=""
-            />
-          </div>
-          <div className="col-sm-6">
-            <h1 className="mx-5 mt-5 fw-bold text-center">Plots</h1>
-            <p className="mt-4 ms-5" style={{ textAlign: "justify" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-              laboriosam error a voluptatem voluptatum consequatur, porro
-              delectus iste pariatur quos illo numquam exercitationem tempora
-              animi est laudantium quis quaerat laborum accusantium accusamus
-              distinctio adipisci libero omnis minima. Laboriosam, quas,
-              voluptatem sed dolore temporibus aut iure optio, atque ipsum
-              corrupti amet voluptate vitae id ratione possimus. Esse et rem
-              accusamus maxime consectetur at, dolor porro impedit beatae qui
-              animi debitis repellendus quo, omnis doloremque corrupti,
-              similique fuga possimus inventore neque ea.
-            </p>
-            <div align="center">
-              <Link
-                to="/plots"
-                className="btn btn-outline-primary rounded-pill px-3 py-2 mt-5"
-              >
-                View All Plots
-              </Link>
+            <div className="col-lg-6 img-hover-zoom img-hover-zoom--brightness d-flex align-items-center pt-5">
+              <img
+                src="./images/5.jpg"
+                className="rounded imgShadow img-fluid"
+                alt=""
+              />
             </div>
           </div>
         </div>
-      </div>
+      </section>
 
-      <div className="container-fluid mb-5 p-5 whiteBackground">
-        <div className="row">
-          <div className="col-sm-6">
-            <h1 className="mx-5 mt-5 fw-bold text-center">Rooms</h1>
-            <p className="mt-4 ms-5" style={{ textAlign: "justify" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-              laboriosam error a voluptatem voluptatum consequatur, porro
-              delectus iste pariatur quos illo numquam exercitationem tempora
-              animi est laudantium quis quaerat laborum accusantium accusamus
-              distinctio adipisci libero omnis minima. Laboriosam, quas,
-              voluptatem sed dolore temporibus aut iure optio, atque ipsum
-              corrupti amet voluptate vitae id ratione possimus. Esse et rem
-              accusamus maxime consectetur at, dolor porro impedit beatae qui
-              animi debitis repellendus quo, omnis doloremque corrupti,
-              similique fuga possimus inventore neque ea.
-            </p>
-            <div align="center">
-              <Link
-                to="/rooms"
-                className="btn btn-outline-primary rounded-pill px-3 py-2 mt-5"
-              >
-                View All Rooms
-              </Link>
+      <section className="plots-section greyBackground py-5">
+        <div className="container mb-5">
+          <div className="row">
+            <h1 className="pt-5 fw-bold text-center">Plots</h1>
+            <div className="col-lg-6 img-hover-zoom img-hover-zoom--brightness d-flex align-content-center pt-5">
+              <img
+                src="./images/a.jpg"
+                className="rounded imgShadow img-fluid"
+                alt=""
+              />
+            </div>
+            <div className="col-lg-6 pt-5">
+              <p className="mt-4" style={{ textAlign: "justify" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                laboriosam error a voluptatem voluptatum consequatur, porro
+                delectus iste pariatur quos illo numquam exercitationem tempora
+                animi est laudantium quis quaerat laborum accusantium accusamus
+                distinctio adipisci libero omnis minima. Laboriosam, quas,
+                voluptatem sed dolore temporibus aut iure optio, atque ipsum
+                corrupti amet voluptate vitae id ratione possimus. Esse et rem
+                accusamus maxime consectetur at, dolor porro impedit beatae qui
+                animi debitis repellendus quo, omnis doloremque corrupti,
+                similique fuga possimus inventore neque ea.
+              </p>
+              <div align="center">
+                <Link
+                  to="/plots"
+                  className="btn btn-outline-warning text-dark rounded px-5 py-2 mt-5 text-uppercase"
+                >
+                  View All Plots
+                </Link>
+              </div>
             </div>
           </div>
-          <div className="col-sm-6 img-hover-zoom img-hover-zoom--brightness">
-            <img
-              src="./images/room3.jpg"
-              className="rounded w-100 ms-4 mt-5 imgShadow"
-              alt=""
-            />
+        </div>
+      </section>
+
+      <section className="rooms-section whiteBackground pt-5">
+        <div className="container mb-5">
+          <div className="row">
+            <h1 className="pt-5 fw-bold text-center">Rooms</h1>
+            <div className="col-lg-6 pt-5">
+              <p className="mt-4" style={{ textAlign: "justify" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                laboriosam error a voluptatem voluptatum consequatur, porro
+                delectus iste pariatur quos illo numquam exercitationem tempora
+                animi est laudantium quis quaerat laborum accusantium accusamus
+                distinctio adipisci libero omnis minima. Laboriosam, quas,
+                voluptatem sed dolore temporibus aut iure optio, atque ipsum
+                corrupti amet voluptate vitae id ratione possimus. Esse et rem
+                accusamus maxime consectetur at, dolor porro impedit beatae qui
+                animi debitis repellendus quo, omnis doloremque corrupti,
+                similique fuga possimus inventore neque ea.
+              </p>
+              <div align="center">
+                <Link
+                  to="/rooms"
+                  className="btn btn-outline-warning text-dark rounded px-5 py-2 mt-5 text-uppercase"
+                >
+                  View All Rooms
+                </Link>
+              </div>
+            </div>
+            <div className="col-lg-6 img-hover-zoom img-hover-zoom--brightness d-flex align-content-center pt-5">
+              <img
+                src="./images/room3.jpg"
+                className="rounded imgShadow img-fluid"
+                alt=""
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </section>
 
-      <div className="container-fluid p-5 greyBackground">
-        <div className="row">
-          <div className="col-sm-6 img-hover-zoom img-hover-zoom--brightness">
-            <img
-              src="./images/flat1.jpg"
-              className="rounded w-100 ms-4 mt-5 imgShadow"
-              alt=""
-            />
-          </div>
-          <div className="col-sm-6">
+      <section className="flats-section greyBackground py-5">
+        <div className="container mb-5">
+          <div className="row">
             <h1 className="mx-5 mt-5 fw-bold text-center">Flats</h1>
-            <p className="mt-4 ms-5" style={{ textAlign: "justify" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-              laboriosam error a voluptatem voluptatum consequatur, porro
-              delectus iste pariatur quos illo numquam exercitationem tempora
-              animi est laudantium quis quaerat laborum accusantium accusamus
-              distinctio adipisci libero omnis minima. Laboriosam, quas,
-              voluptatem sed dolore temporibus aut iure optio, atque ipsum
-              corrupti amet voluptate vitae id ratione possimus. Esse et rem
-              accusamus maxime consectetur at, dolor porro impedit beatae qui
-              animi debitis repellendus quo, omnis doloremque corrupti,
-              similique fuga possimus inventore neque ea.
-            </p>
-            <div align="center">
-              <Link
-                to="/flats"
-                className="btn btn-outline-primary rounded-pill px-3 py-2 mt-5"
-              >
-                View All Flats
-              </Link>
+            <div className="col-lg-6 img-hover-zoom img-hover-zoom--brightness d-flex align-content-center pt-5">
+              <img
+                src="./images/flat1.jpg"
+                className="rounded imgShadow img-fluid"
+                alt=""
+              />
+            </div>
+            <div className="col-lg-6 pt-5">
+              <p className="mt-4" style={{ textAlign: "justify" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                laboriosam error a voluptatem voluptatum consequatur, porro
+                delectus iste pariatur quos illo numquam exercitationem tempora
+                animi est laudantium quis quaerat laborum accusantium accusamus
+                distinctio adipisci libero omnis minima. Laboriosam, quas,
+                voluptatem sed dolore temporibus aut iure optio, atque ipsum
+                corrupti amet voluptate vitae id ratione possimus. Esse et rem
+                accusamus maxime consectetur at, dolor porro impedit beatae qui
+                animi debitis repellendus quo, omnis doloremque corrupti,
+                similique fuga possimus inventore neque ea.
+              </p>
+              <div className="text-center">
+                <Link
+                  to="/flats"
+                  className="btn btn-outline-warning text-dark rounded px-5 py-2 mt-5 text-uppercase"
+                >
+                  View All Flats
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
