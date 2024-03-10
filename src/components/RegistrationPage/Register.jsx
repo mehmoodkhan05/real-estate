9c7fb9c89f75560b5b9fa0a7a4813c5d7c91d8d8
Mehmood Khan <mehmadk05@gmail.com>
2024-03-11 02:58:52 +0500

    registration page changed

--- src/components/RegistrationPage/Register.jsx
+++ src/components/RegistrationPage/Register.jsx
@@ -1,7 +1,8 @@
+import { Container } from "react-bootstrap";
 import "./register.css";
+import { Link } from "react-router-dom";
 // import axios from "axios";
 // import { useState } from "react";
-import { Link } from "react-router-dom";
 
 function Register() {
   // const navigate = useNavigate();
@@ -23,169 +24,89 @@ function Register() {
   // };
   return (
     <>
-      {/* <section className="bg-image">
-        <div className="mask d-flex align-items-center h-100 gradient-custom-3">
-          <div className="container h-100 mt-5 mb-5 ">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-              <div className="col-12 col-md-9 col-lg-7 col-xl-6">
-                <div className="card regInner">
-                  <div className="card-body p-5">
-                    <h2 className="text-uppercase text-center text-dark mb-5 regHeading">
-                      Create an account
-                    </h2>
-
-                    <form method="POST">
-                      <div className="form-outline mb-4">
-                        <input
-                          type="text"
-                          id="form3Example1cg"
-                          name="name"
-                          onChange={handleChange}
-                          className="form-control form-control-lg regForm"
-                          required
-                        />
-                        <label className="form-label" htmlFor="form3Example1cg">
-                          Your Name
-                        </label>
-                      </div>
-
-                      <div className="form-outline mb-4">
-                        <input
-                          type="email"
-                          name="email"
-                          onChange={handleChange}
-                          id="form3Example3cg"
-                          className="form-control form-control-lg regForm"
-                          required
-                        />
-                        <label className="form-label" htmlFor="form3Example3cg">
-                          Your E-mail
-                        </label>
-                      </div>
-
-                      <div className="form-outline mb-4">
-                        <input
-                          type="password"
-                          id="form3Example4cg"
-                          name="password"
-                          onChange={handleChange}
-                          className="form-control form-control-lg regForm"
-                          required
-                        />
-                        <label className="form-label" htmlFor="form3Example4cg">
-                          Password
-                        </label>
-                      </div>
-
-                      <div className="form-outline mb-4">
-                        <input
-                          type="number"
-                          name="contact"
-                          onChange={handleChange}
-                          className="form-control form-control-lg regForm"
-                          required
-                        />
-                        <label className="form-label" htmlFor="form3Example4cdg">
-                          Phone #
-                        </label>
-                      </div>
-
-                      <div className="form-outline mb-4">
-                        <input
-                          type="text"
-                          name="address"
-                          onChange={handleChange}
-                          className="form-control form-control-lg regForm"
-                          required
-                        />
-                        <label className="form-label" htmlFor="form3Example4cdg">
-                          Address
-                        </label>
-                      </div>
-
-                      <div className="d-flex justify-content-center mt-5">
-                        <button onClick={handleSubmit} className="button">
-                          Submit
-                        </button>
-                      </div>
-
-                      <div>
-                        <p className="mt-5 signPRG text-center">
-                          Already Have an Account!
-                          <Link
-                            to="/signin"
-                            className="text-dark fw-bold text-decoration-none customSign"
-                          >
-                            Sign In
-                          </Link>
-                        </p>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section> */}
-
-      <div className="container signCustomContainer mt-5 mb-5">
-        <div className="signFormContainer">
-          <div className="overlay-sign">
-            <div className="overlay">
-              <div className="overlay-panel overlay-right">
-                <img src="./images/login.jpeg" alt="" />
-                <h1>Welcome Back!</h1>
-                <p className="signPRG">
-                  To keep connetced with us please login with your personal
-                  info.
-                </p>
-              </div>
+      <Container className="d-flex justify-content-center mt-5 mb-5">
+        <div className="registration-page">
+          <div className="reg_overlay">
+            <div className="reg_overlay-image"></div>
+            <div className="reg_overlay-panel text-center">
+              <h1>Welcome Back!</h1>
+              <p className="reg_PRG">
+                To keep connetced with us please login with your personal info.
+              </p>
+              <Link to="/signin" className="btn btn-w-108 mt-3">
+                Login
+              </Link>
             </div>
           </div>
 
-          <form method="POST" className="signForm">
-            <div className="main_header">
-              <h1 className="signHeading mt-5">Login</h1>
+          <form method="POST" className="reg_form">
+            <div className="reg_heading">
+              <h1 className="mt-5">Create Account</h1>
             </div>
-            <input
-              type="email"
-              name="email"
-              // onChange={handleChange}
-              placeholder="Email"
-              className="signInput"
-              required
-            />
-            <input
-              type="password"
-              // onChange={handleChange}
-              name="password"
-              placeholder="Password"
-              className="signInput"
-              required
-            />
+            <label className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                // onChange={handleChange}
+                required
+              />
+              <span className="contactTxt">Enter Your Name</span>
+              <span className="border"></span>
+            </label>
+
+            <label className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                // onChange={handleChange}
+                required
+              />
+              <span htmlFor="" className="contactTxt">
+                Enter Your E-mail
+              </span>
+              <span className="border"></span>
+            </label>
+
+            <label className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                // onChange={handleChange}
+                required
+              />
+              <span htmlFor="" className="contactTxt">
+                Enter Your Password
+              </span>
+              <span className="border"></span>
+            </label>
+
+            <label className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                // onChange={handleChange}
+                required
+              />
+              <span htmlFor="" className="contactTxt">
+                Confirm Your E-mail
+              </span>
+              <span className="border"></span>
+            </label>
 
             <button
               // onClick={handleSubmit}
               type="submit"
-              className="signButton Sign-a"
+              className="btn btn-w-108 text-uppercase mt-5"
             >
-              Login
+              Sign up
             </button>
-            <div>
-              <p className="mt-5 signPRG">
-                Don't have Account?{" "}
-                <Link
-                  to="/register"
-                  className="text-dark fw-bold text-decoration-none customSign"
-                >
-                  Sign Up
-                </Link>
-              </p>
-            </div>
           </form>
         </div>
-      </div>
+      </Container>
     </>
   );
 }
